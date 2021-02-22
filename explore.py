7d8dbabbf0a434ce793939b75e26987ad75981b5
Ddelcastillo <d.delcastillo@uniandes.edu.co>
2021-02-22 11:05:38 -0500

    Exploración de archivos

--- explore.py
+++ explore.py
@@ -7,11 +7,14 @@ import xml.etree.ElementTree as ET
 
 # %% Displaying images and annotations
 # Taken from https://docs.python.org/3/library/os.html
-n = 4
+n = 4   # Number of images to select
+# Randomly selects n images form the database.
 selected_images = []
 for root, dirs, files in os.walk(os.path.join('.', 'images')):
     selected_images = random.sample(files, n)
 # Taken from https://docs.python.org/3/library/xml.etree.elementtree.html
+# Based on the randomly selected images, gets the instance information
+# for each respective image XML file. Makes a string to later plot.
 selected_annotations = []
 for i in range(n):
     tree = ET.parse(os.path.join('annotations', selected_images[i].split('.')[0] + '.xml'))
@@ -21,7 +24,8 @@ for i in range(n):
         instances += tags.find('name').text + ', '
     instances = instances[:-2]
     selected_annotations.append(instances)
-# Image display
+# Displays the image (left) with it's respective list of detected instances
+# (annotations, right). Displays and shows the image.
 fig, axs = plt.subplots(n, 2)
 for i in range(n):
     img = io.imread(os.path.join('images', selected_images[i]))
@@ -32,3 +36,27 @@ for i in range(n):
 fig.suptitle('Imágenes y sus correspondientes instancias detectadas (anotaciones)')
 fig.show()
 fig.savefig('sample_database_images.png')
+input('Press enter to continue...')
+
+# %% Class instance exploration
+
+# Counts all instances for each image in for all the XML files. Stores them
+# in a dictionary and them plots the frequencies in a bar graph.
+count = {}
+for root, dirs, files in os.walk(os.path.join('.', 'annotations')):
+    for annotation in files:
+        tree = ET.parse(os.path.join('annotations',annotation))
+        root = tree.getroot()
+        for tags in root.findall('object'):
+            instance = tags.find('name').text
+            if instance in count:
+                count[instance] += 1
+            else:
+                count[instance] = 1
+# Bar graph generation
+fig, ax = plt.subplots(1)
+ax.bar(list(count.keys()), list(count.values()))
+fig.suptitle('Distribución del número de instancias (clases)')
+fig.show()
+fig.savefig('instance_count.png')
+input('Press enter to finalize.')
